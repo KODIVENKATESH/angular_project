4739edd78333f0bfeddb0eb0d9baaa4814f8c5ce
KODIVENKATESH <venky246venkatesh@gmail.com>
2024-11-12 22:08:05 +0530

    add-task

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,17 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
-// type User = {
-//   id:string;
-//   name:string;
-//   avatar:string;
-// }
-interface User {
-  id:string;
-  name:string;
-  avatar:string;
-}
-
-
+import { User } from './user.model';
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -21,6 +9,7 @@ interface User {
 })
 export class UserComponent {
   @Input({required:true})  user!: User;
+  @Input({required:true}) selected !: boolean;
 
   @Output() select = new EventEmitter()
   get imagePath() {
